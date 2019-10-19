ad1a5fdc9837b17d0955db60aac94011578865c7
brian23-eng <b.odhiambo.bo@gmail.com>
2019-10-19 17:29:14 +0300

    install flask-script, import manager and server from flask and include the manager shell

---
+++ manage.py
@@ -0,0 +1,19 @@
+from app import create_app, db
+from flask_script import Manager, Server
+
+
+@manager.command
+def test():
+    """
+    Run the unittests
+    """
+    import unittest
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Role = Role )
+
+if __name__ == '__main__':
+    manager.run()
