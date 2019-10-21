bfa661b5767b00129cac3e4772ccc454572c0b25
brian23-eng <b.odhiambo.bo@gmail.com>
2019-10-21 13:55:48 +0300

    create methods to verify and set password

--- app/models.py
+++ app/models.py
@@ -1,23 +1,34 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    hash_pass = db.Column(db.String(255) )
 
+    
+   
+    
+    
+    @property
+    def password(self):
+        raise AttributeError("You can not read password attribute")
+    
+    @password.setter
+    def password(self,password):
+        self.hash_pass = generate_password_hash(password)
+        
+    def set_password(self, password):
+        self.hash_pass = generate_password_hash(password)
+        
+    def verify_password(self, password):
+        return check_password_hash(self.hash_pass, password)
+    
     
     def __repr__(self):
         return f'User {self.username}'
+        
     
     
-class Pitch(db.Model):
-    __tablename__ = 'pitches'
     
-    id = db.Column(db.Integer, primary_key = True)
-    pitch_content = db.Column(db.String())
-    pitch_category = db.Column(db.String(255))
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    
-    def save_pitch(self):
-        db.session.add(self)
-        db.session.commit()
