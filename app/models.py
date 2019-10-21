2128c966080a90fa72286c908758cc4746790fb3
brian23-eng <b.odhiambo.bo@gmail.com>
2019-10-21 10:29:17 +0300

    create a class for pitch with four columns for id, pitch category and pitch content and include a column for foreign-key

--- app/models.py
+++ app/models.py
@@ -4,7 +4,20 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
     
     def __repr__(self):
         return f'User {self.username}'
+    
+    
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    pitch_content = db.Column(db.String())
+    pitch_category = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
