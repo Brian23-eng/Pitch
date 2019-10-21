a5275f7856ce00571fe68b72864974d2b777af05
brian23-eng <b.odhiambo.bo@gmail.com>
2019-10-21 20:44:12 +0300

    add test for comment instatnces, create functions for sending email to users

--- config.py
+++ config.py
@@ -2,17 +2,32 @@ import os
 
 class Config:
     
-      
     '''
     Describes the general configurations
-    
     '''
-    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DATABASE_PASS = os.environ.get('DATABASE_PASS')
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://brian:brayo13@localhost/pitch'
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
+    #emails configuration
+    
+    MAIL_SERVER=os.environ.get('MAIL_SERVER')
+    MAIL_PORT=os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS')
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+    
+    
+    
+    #Simple MDE configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    
+    @staticmethod
+    def init_app(app):
+        pass
     
    
   
@@ -27,16 +42,40 @@ class ProdConfig(Config):
         Config: The parent configuration class with general configuration settings
     '''
     
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
+    
     
     
 class DevConfig(Config):
-    
 
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://brian:brayo13@localhost/pitch'
+    
     DEBUG = True
+    
+class TestConfig(Config):
+    
+    '''
+    Test configuration child class
+    
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    
+    DATABASE_PASS = os.environ.get('DATABASE_PASS')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://brian:brayo13@localhost/pitch'
+    
+    
     
 config_options = {
     'development':DevConfig,
     'production':ProdConfig,
+    'test': TestConfig
 }
         
     
