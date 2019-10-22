41ebdb52cf6a5813527a1f57b1c96c01c06fe393
brian23-eng <b.odhiambo.bo@gmail.com>
2019-10-22 20:40:43 +0300

    export mail username, mail password, secret key sqlalchemy database uri in file start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-export SECRET_KEY='bEtteR23'
-export MAIL_USERNAME= 'b.odhiambo.bo@gmail.com'
-export MAIL_PASSWORD='stephen13'
+export MAIL_USERNAME ='b.odhiambo.bo@gmail.com'
+export  MAIL_PASSWORD ='stephen13'
+export SECRET_KEY='bran'
+SQLALCHEMY_DATABASE_URI='DATABASE_URL'
+python3.6 manage.py server
